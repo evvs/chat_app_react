1e8107ca134b1ad9df2bfd737d3b6fc1bf6b1244
Evgeniy <va.evgenii@gmail.com>
2020-08-06 17:23:09 +0300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Channels from "./Channels";
+
+const App = () => (
+    <div className="col-5">
+      <Channels />
+    </div>
+);
+
+export default App;
---
+++ src/components/Channels.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Channels = (props) => {
+    return (
+      <div>Test</div>
+    );
+};
+
+export  default  Channels;
